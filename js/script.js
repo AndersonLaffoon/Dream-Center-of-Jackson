be1b6f7c8e394c76add807cb23a2f11dfcaa017d
Corwin Bridges <corwin.bridges@gmail.com>
2021-11-01 10:26:59 -0500

    gg

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function addFlexWrap() {
-    var x = document.getElementById('addFlexWrap');
-    if (x.style.flex-wrap == 'nowrap'){
-        x.style.flex-wrap == 'wrap'
-    }else{
-        x.style.flex-wrap == 'none'
-    }
-}
