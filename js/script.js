564d4082263453ba7c6dd0a91f275bc08b56366f
Corwin Bridges <corwin.bridges@gmail.com>
2021-11-15 10:26:10 -0600

    dd
    Co-Authored-By: Vraj Patel <88333545+vraj-pate1@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ $(function () {
       });
 });
 
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+	$(".logo-zoom img").css({
+		transform: 'translate3d(-0%, -'+(scroll/100)+'%, 0) scale('+(100 + scroll/0.08)/100+')',
+	});
+});
