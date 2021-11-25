d33a6e25bac408bb2c8ad2ac049c86b86e1de6f6
Corwin Bridges <corwin.bridges@gmail.com>
2021-11-25 07:26:33 -0600

    ddf

--- js/jquery.js
+++ js/jquery.js
@@ -20,57 +20,57 @@ $(function () {
   const ZOOM_SPEED = 0.3;
 
   // Zoom In on Mouse Wheel Up & Zoom Out on Mouse Wheel Down
-  $(".zoom").on("wheel", function (e) {
-    if (e.originalEvent.deltaY !== 0) {
-      if (e.originalEvent.deltaY < 0) {
-        $(".zoom").css({
-          transform: `scale(${(zoom += ZOOM_SPEED)})`,
-        });
-      } else if (zoom > 0.1) {
-        $(".zoom").css({
-          transform: `scale(${(zoom -= ZOOM_SPEED)})`,
-        });
-      }
-    }
-  });
+  // $(".zoom").on("wheel", function (e) {
+  //   if (e.originalEvent.deltaY !== 0) {
+  //     if (e.originalEvent.deltaY < 0) {
+  //       $(".zoom").css({
+  //         transform: `scale(${(zoom += ZOOM_SPEED)})`,
+  //       });
+  //     } else if (zoom > 0.1) {
+  //       $(".zoom").css({
+  //         transform: `scale(${(zoom -= ZOOM_SPEED)})`,
+  //       });
+  //     }
+  //   }
+  // });
 
   // Zoom In Button
-  $(".zoomIn").on("click", function () {
-    $(".zoom").css({
-      transform: `scale(${(zoom += ZOOM_SPEED)})`,
-    });
-  });
+  // $(".zoomIn").on("click", function () {
+  //   $(".zoom").css({
+  //     transform: `scale(${(zoom += ZOOM_SPEED)})`,
+  //   });
+  // });
 
-  // Zoom Out Button
-  $(".zoomOut").on("click", function () {
-    if (zoom > 0.1) {
-      $(".zoom").css({
-        transform: `scale(${(zoom -= ZOOM_SPEED)})`,
-      });
-    }
-  });
+  // // Zoom Out Button
+  // $(".zoomOut").on("click", function () {
+  //   if (zoom > 0.1) {
+  //     $(".zoom").css({
+  //       transform: `scale(${(zoom -= ZOOM_SPEED)})`,
+  //     });
+  //   }
+  // });
 
-  // Reset Zoom Button
-  $(".resetZoom").on("click", function () {
-    $(".zoom").css({
-      transform: `scale(${(zoom = 1)})`,
-    });
-  });
+  // // Reset Zoom Button
+  // $(".resetZoom").on("click", function () {
+  //   $(".zoom").css({
+  //     transform: `scale(${(zoom = 1)})`,
+  //   });
+  // });
 
-  // Make Element Draggable
-  $(".draggable").draggable();
-  $(".draggable").data({
-    originalLeft: $(".draggable").css("left"),
-    origionalTop: $(".draggable").css("top"),
-  });
+  // // Make Element Draggable
+  // $(".draggable").draggable();
+  // $(".draggable").data({
+  //   originalLeft: $(".draggable").css("left"),
+  //   origionalTop: $(".draggable").css("top"),
+  // });
 
-  // Reset Element's Position
-  $(".resetPos").on("click", function () {
-    $(".draggable").css({
-      left: $(".draggable").data("originalLeft"),
-      top: $(".draggable").data("origionalTop"),
-    });
-  });
+  // // Reset Element's Position
+  // $(".resetPos").on("click", function () {
+  //   $(".draggable").css({
+  //     left: $(".draggable").data("originalLeft"),
+  //     top: $(".draggable").data("origionalTop"),
+  //   });
+  // });
 
   // Show Current Slide Number out of Total Slides
   var totalItems = $(".slideNumber").length;
@@ -91,4 +91,73 @@ $(function () {
   $(".review-header").fitText(1.2);
   $(".index-header").fitText(1.4);
   $(".index-subheader").fitText(3.5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  var scale = 1,
+    panning = false,
+    xoff = 0,
+    yoff = 0,
+    start = {
+      x: 0,
+      y: 0,
+    }
+
+  $.fn.setTransform = function () {
+    $(".zoom").css({
+      transform: "translate(" + xoff + "px, " + yoff + "px) scale(" + scale + ")"
+    });
+  }
+
+  $(".zoom").mousedown(function (e) {
+    e.preventDefault();
+    start = {
+      x: e.clientX - xoff,
+      y: e.clientY - yoff,
+    };
+    panning = true;
+  })
+
+  $(".zoom").mouseup(function (e) {
+    panning = false;
+  })
+
+  $(".zoom").mousemove(function (e) {
+    e.preventDefault();
+    if (!panning) {
+      return;
+    }
+    xoff = e.clientX - start.x;
+    yoff = e.clientY - start.y;
+    $.setTransform();
+  })
+
+  $(".zoom").on("wheel", function (e) {
+    if (e.originalEvent.deltaY !== 0) {
+      e.preventDefault();
+      var xs = (e.clientX - xoff) / scale,
+        ys = (e.clientY - yoff) / scale,
+        delta = e.wheelDelta ? e.wheelDelta : -e.deltaY;
+
+      delta > 0 ? (scale *= 1.2) : (scale /= 1.2);
+
+      xoff = e.clientX - xs * scale;
+      yoff = e.clientY - ys * scale;
+
+      $.fn.setTransform();
+    }
+  })
 });
--- js/scripts.js
+++ js/scripts.js
@@ -58,3 +58,90 @@ function closeFullscreen() {
 	compress.innerHTML = "<i class='fas fa-expand fa-2x lightboxhover'></i>";
 	compress.id = "Expand";
 }
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () {
+	progressScrollBar()
+};
+
+function progressScrollBar() {
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	var scrolled = (winScroll / height) * 100;
+	document.getElementById("progressbar").style.width = scrolled + "%";
+}
+
+document.addEventListener("gesturestart", function (e) {
+	e.preventDefault();
+	// special hack to prevent zoom-to-tabs gesture in safari
+	document.body.style.zoom = 0.99;
+});
+
+document.addEventListener("gesturechange", function (e) {
+	e.preventDefault();
+	// special hack to prevent zoom-to-tabs gesture in safari
+	document.body.style.zoom = 0.99;
+});
+
+document.addEventListener("gestureend", function (e) {
+	e.preventDefault();
+	// special hack to prevent zoom-to-tabs gesture in safari
+	document.body.style.zoom = 0.99;
+});
+
+
+// Zoom in on cursor
+// var scale = 1,
+// 	panning = false,
+// 	xoff = 0,
+// 	yoff = 0,
+// 	start = {
+// 		x: 0,
+// 		y: 0
+// 	},
+// 	doc = document.querySelectorAll(".zoom");
+	
+// function setTransform() {
+// 	doc.style.transform =
+// 		"translate(" + xoff + "px, " + yoff + "px) scale(" + scale + ")";
+// }
+
+// doc.onmousedown = function (e) {
+// 	e.preventDefault();
+// 	start = {
+// 		x: e.clientX - xoff,
+// 		y: e.clientY - yoff
+// 	};
+// 	panning = true;
+// };
+
+// doc.onmouseup = function (e) {
+// 	panning = false;
+// };
+
+// doc.onmousemove = function (e) {
+// 	e.preventDefault();
+// 	if (!panning) {
+// 		return;
+// 	}
+// 	xoff = e.clientX - start.x;
+// 	yoff = e.clientY - start.y;
+// 	setTransform();
+// };
+
+// doc.onwheel = function (e) {
+// 	e.preventDefault();
+// 	// take the scale into account with the offset
+// 	var xs = (e.clientX - xoff) / scale,
+// 		ys = (e.clientY - yoff) / scale,
+// 		delta = e.wheelDelta ? e.wheelDelta : -e.deltaY;
+
+// 	// get scroll direction & set zoom level
+// 	delta > 0 ? (scale *= 1.2) : (scale /= 1.2);
+
+// 	// reverse the offset amount with the new scale
+// 	xoff = e.clientX - xs * scale;
+// 	yoff = e.clientY - ys * scale;
+
+// 	setTransform();
+// };
