096586b07fe6cc30cf22b6f5505b170bf048a237
Corwin Bridges <corwin.bridges@gmail.com>
2021-11-27 09:58:07 -0600

    f

--- js/jquery.js
+++ js/jquery.js
@@ -15,63 +15,6 @@ $(function () {
     $(this).children(".dropdown-menu").stop().slideUp("fast");
   });
 
-  // Scroll Zoom In & Out original script via Dotted Squirrel (https://www.dottedsquirrel.com/zoom-scroll-tutorial/)
-  let zoom = 1;
-  const ZOOM_SPEED = 0.3;
-
-  // Zoom In on Mouse Wheel Up & Zoom Out on Mouse Wheel Down
-  // $(".zoom").on("wheel", function (e) {
-  //   if (e.originalEvent.deltaY !== 0) {
-  //     if (e.originalEvent.deltaY < 0) {
-  //       $(".zoom").css({
-  //         transform: `scale(${(zoom += ZOOM_SPEED)})`,
-  //       });
-  //     } else if (zoom > 0.1) {
-  //       $(".zoom").css({
-  //         transform: `scale(${(zoom -= ZOOM_SPEED)})`,
-  //       });
-  //     }
-  //   }
-  // });
-
-  // Zoom In Button
-  // $(".zoomIn").on("click", function () {
-  //   $(".zoom").css({
-  //     transform: `scale(${(zoom += ZOOM_SPEED)})`,
-  //   });
-  // });
-
-  // // Zoom Out Button
-  // $(".zoomOut").on("click", function () {
-  //   if (zoom > 0.1) {
-  //     $(".zoom").css({
-  //       transform: `scale(${(zoom -= ZOOM_SPEED)})`,
-  //     });
-  //   }
-  // });
-
-  // // Reset Zoom Button
-  // $(".resetZoom").on("click", function () {
-  //   $(".zoom").css({
-  //     transform: `scale(${(zoom = 1)})`,
-  //   });
-  // });
-
-  // // Make Element Draggable
-  // $(".draggable").draggable();
-  // $(".draggable").data({
-  //   originalLeft: $(".draggable").css("left"),
-  //   origionalTop: $(".draggable").css("top"),
-  // });
-
-  // // Reset Element's Position
-  // $(".resetPos").on("click", function () {
-  //   $(".draggable").css({
-  //     left: $(".draggable").data("originalLeft"),
-  //     top: $(".draggable").data("origionalTop"),
-  //   });
-  // });
-
   // Show Current Slide Number out of Total Slides
   var totalItems = $(".slideNumber").length;
   var currentIndex = $("div.active").index() + 1;
@@ -92,7 +35,6 @@ $(function () {
   $(".index-header").fitText(1.4);
   $(".index-subheader").fitText(3.5);
 
-  // Zoom In on Cursor
   var scale = 1,
     panning = false,
     xoff = 0,
@@ -100,15 +42,15 @@ $(function () {
     start = {
       x: 0,
       y: 0,
-    }
+    };
 
   $.fn.setTransform = function () {
     $(".zoom").css({
       transform: "translate(" + xoff + "px, " + yoff + "px) scale(" + scale + ")"
     });
   }
-
-  $(".zoom").on("mousedown", function (e) {
+  // Allows Panning with Mouse
+  $(".modal").on("mousedown", function (e) {
     e.preventDefault();
     start = {
       x: e.clientX - xoff,
@@ -117,11 +59,11 @@ $(function () {
     panning = true;
   })
 
-  $(".zoom").on("mouseup", function (e) {
+  $(".modal").on("mouseup", function (e) {
     panning = false;
   })
 
-  $(".zoom").on("mousemove", function (e) {
+  $(".modal").on("mousemove", function (e) {
     e.preventDefault();
     if (!panning) {
       return;
@@ -131,43 +73,97 @@ $(function () {
     $.fn.setTransform();
   })
 
-  // $(".zoom").on("wheel", function (e) {
-  //   e.preventDefault();
-	// // take the scale into account with the offset
-  //   var xs = (e.clientX - xoff) / scale,
-  //     ys = (e.clientY - yoff) / scale;
-  //   if (e.originalEvent.deltaY !== 0) {
-  //     if (e.originalEvent.deltaY < 0) {
-  //       // wheeled up
-  //       scale *= 1.2;
-  //       delta = e.deltaY;
-  //     } else {
-  //       // wheeled down
-  //       scale /= 1.2;
-  //       delta = -e.deltaY;
+  // Allows Panning with Touch
+  $(".zoom").on("touchstart", function (e) {
+    e.preventDefault();
+    start = {
+      x: e.touches[0].clientX - xoff,
+      y: e.touches[0].clientY - yoff,
+    };
+    panning = true;
+  })
 
-  //     }
-  //  // reverse the offset amount with the new scale
-  //     xoff = e.clientX - xs * scale;
-  //     yoff = e.clientY - ys * scale;
+  $(".zoom").on("touchend", function (e) {
+    panning = false;
+  })
 
-  //     $.fn.setTransform();
-  //   }
-  // })
+  $(".zoom").on("touchmove", function (e) {
+    e.preventDefault();
+    if (!panning) {
+      return;
+    }
+    xoff = e.touches[0].clientX - start.x;
+    yoff = e.touches[0].clientY - start.y;
+    $.fn.setTransform();
+  })
+
+  // Zoom In & Out on Cursor
+  $(".zoom").on("wheel", function (e) {
+    e.preventDefault();
+    // take the scale into account with the offset
+    var xs = (e.clientX - xoff) / scale,
+      ys = (e.clientY - yoff) / scale;
+    if (e.originalEvent.deltaY !== 0) {
+      if (e.originalEvent.deltaY < 0) {
+        // wheel up / zoom in
+        scale *= 1.2;
+      } else {
+        // wheel down / zoom out
+        scale /= 1.2;
+      }
+      // reverse the offset amount with the new scale
+      xoff = e.clientX - xs * scale;
+      yoff = e.clientY - ys * scale;
+      $.fn.setTransform();
+    }
+  })
 
   // Reset Zoom Button
   $(".resetZoom").on("click", function () {
     $(".zoom").css({
-      transform: "translate(" + (xoff=0) + "px, " + (yoff=0) + "px) scale(" + (scale=1) + ")"
+      transform: "translate(" + (xoff = 0) + "px, " + (yoff = 0) + "px) scale(" + (scale = 1) + ")"
     });
+  });
 
-    $(".zoom").on("mousedown", function (e) {
-      e.preventDefault();
-      start = {
-        x: e.clientX - (xoff=0),
-        y: e.clientY - (yoff=0),
-      };
-      panning = true;
-    })
+  // Zoom In Button for Center
+  $(".zoomIn").on("click", function (e) {
+    e.preventDefault();
+    // Get Center of element
+    var $zoom = $(".carousel-item.active .zoom"),
+      centerX = $zoom.width() / 2,
+      headerHeight = $(".modal-header").height(),
+      bodyHeight = $zoom.height() / 2,
+      centerY = headerHeight + bodyHeight,
+      // take the scale into account with the offset
+      xs = (centerX - xoff) / scale,
+      ys = (centerY - yoff) / scale;
+    console.log(centerX, centerY);
+    console.log();
+    scale *= 1.2;
+    // reverse the offset amount with the new scale
+    xoff = centerX - xs * scale;
+    yoff = centerY - ys * scale;
+
+    $.fn.setTransform();
+  });
+
+  // Zoom Out Button for Center
+  $(".zoomOut").on("click", function (e) {
+    e.preventDefault();
+    // Get Center of element
+    var $zoom = $(".carousel-item.active .zoom"),
+      centerX = $zoom.width() / 2,
+      headerHeight = $(".modal-header").height(),
+      bodyHeight = $zoom.height() / 2,
+      centerY = headerHeight + bodyHeight,
+      // take the scale into account with the offset
+      xs = (centerX - xoff) / scale,
+      ys = (centerY - yoff) / scale;
+    scale /= 1.2;
+    // reverse the offset amount with the new scale
+    xoff = centerX - xs * scale;
+    yoff = centerY - ys * scale;
+
+    $.fn.setTransform();
   });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -60,16 +60,16 @@ function closeFullscreen() {
 }
 
 // When the user scrolls the page, execute myFunction 
-window.onscroll = function () {
-	progressScrollBar()
-};
+// window.onscroll = function () {
+// 	progressScrollBar()
+// };
 
-function progressScrollBar() {
-	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-	var scrolled = (winScroll / height) * 100;
-	document.getElementById("progressbar").style.width = scrolled + "%";
-}
+// function progressScrollBar() {
+// 	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+// 	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+// 	var scrolled = (winScroll / height) * 100;
+// 	document.getElementById("progressbar").style.width = scrolled + "%";
+// }
 
 document.addEventListener("gesturestart", function (e) {
 	e.preventDefault();
@@ -88,53 +88,3 @@ document.addEventListener("gestureend", function (e) {
 	// special hack to prevent zoom-to-tabs gesture in safari
 	document.body.style.zoom = 0.99;
 });
-
-
-// // Zoom in on cursor
-// var scale = 1,
-//     panning = false,
-//     xoff = 0,
-//     yoff = 0,
-//     start = {x: 0, y: 0},
-//     doc = document.getElementById("x");
-
-// function setTransform() {
-//   doc.style.transform = "translate(" + xoff + "px, " + yoff + "px) scale(" + scale + ")";
-// }
-
-// doc.onmousedown = function(e) {
-//   e.preventDefault();
-//   start = {x: e.clientX - xoff, y: e.clientY - yoff};    
-//   panning = true;
-// }
-
-// doc.onmouseup = function(e) {
-//   panning = false;
-// }
-
-// doc.onmousemove = function(e) {
-//   e.preventDefault();         
-//   if (!panning) {
-//     return;
-//   }
-//   xoff = (e.clientX - start.x);
-//   yoff = (e.clientY - start.y);
-//   setTransform();
-// }
-
-// doc.onwheel = function(e) {
-//     e.preventDefault();
-//     // take the scale into account with the offset
-//     var xs = (e.clientX - xoff) / scale,
-//         ys = (e.clientY - yoff) / scale,
-//         delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
-
-//     // get scroll direction & set zoom level
-//     (delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
-
-//     // reverse the offset amount with the new scale
-//     xoff = e.clientX - xs * scale;
-//     yoff = e.clientY - ys * scale;
-
-//     setTransform();          
-// }
