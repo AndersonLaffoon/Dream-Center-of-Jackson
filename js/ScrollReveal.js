1ca895b2c5cef22a0c9fc72d14f410bac37c5d62
Corwin Bridges <corwin.bridges@gmail.com>
2021-12-08 09:42:57 -0600

    dd

--- js/ScrollReveal.js
+++ js/ScrollReveal.js
@@ -1,39 +1,53 @@
-// ScrollReveal().reveal(".headline", {
-//     duration: 700,
-//     distance: "-2.5rem",
-// });
+ScrollReveal().reveal(".headline", {
+    duration: 700,
+    distance: "-2.5rem",
+});
 
-// ScrollReveal().reveal(".imgline1", {
-//     interval: 100,
-//     duration: 700,
-//     easing: "ease-in-out",
-//     distance: "-2.5rem",
-// });
+ScrollReveal().reveal(".imgline1", {
+    interval: 100,
+    duration: 700,
+    easing: "ease-in-out",
+    distance: "-2.5rem",
+});
 
-// ScrollReveal().reveal(".imgline2", {
-//   interval: 100,
-//   duration: 700,
-//   easing: "ease-in-out",
-//   distance: "-2.5rem",
-// });
+ScrollReveal().reveal(".imgline2", {
+    interval: 100,
+    duration: 700,
+    easing: "ease-in-out",
+    distance: "-2.5rem",
+});
 
-// ScrollReveal().reveal(".imgline3", {
-//   interval: 100,
-//   duration: 700,
-//   easing: "ease-in-out",
-//   distance: "-2.5rem",
-// });
+ScrollReveal().reveal(".imgline3", {
+    interval: 100,
+    duration: 700,
+    easing: "ease-in-out",
+    distance: "-2.5rem",
+});
 
-// ScrollReveal().reveal(".imgline4", {
-//   interval: 100,
-//   duration: 700,
-//   easing: "ease-in-out",
-//   distance: "-2.5rem",
-// });
+ScrollReveal().reveal(".imgline4", {
+    interval: 100,
+    duration: 700,
+    easing: "ease-in-out",
+    distance: "-2.5rem",
+});
 
-// ScrollReveal().reveal(".multi-item-carousel-reveal", {
-//     delay: 950,
-//     duration: 700,
-//     easing: "ease-in-out",
-//     distance: "-2.5rem",
-// });
+ScrollReveal().reveal(".multi-item-carousel-reveal", {
+    delay: 950,
+    duration: 700,
+    easing: "ease-in-out",
+    distance: "-2.5rem",
+});
+
+ScrollReveal().reveal(".section-reveal", {
+    interval: 600,
+    duration: 500,
+    easing: "ease-in-out",
+    distance: "-2.5rem",
+});
+
+ScrollReveal().reveal(".card-reveal", {
+    interval: 200,
+    duration: 500,
+    easing: "ease-in-out",
+    distance: "-2.5rem",
+});
